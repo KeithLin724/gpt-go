3b59893f24c3159aa0c735c734c0c0e14b4a7d04
LinBR <zxawszx@gmail.com>
2023-12-11 22:10:10 +0800

    New struct "Log" for level "INFO" and "ERROR"

--- pkg/gpt_log.go
+++ pkg/gpt_log.go
@@ -1,11 +1,44 @@
 package pkg
 
 import (
-	"time"
+	"log/slog"
+	"os"
 )
 
 type Log struct {
-	Time  time.Time
-	Msg   string
-	Level string
+	infoLogger  *slog.Logger
+	errorLogger *slog.Logger
+}
+
+func NewLog() *Log {
+	infoOption := slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}
+	infoHandler := slog.NewTextHandler(os.Stdout, &infoOption)
+	infoLogger := slog.New(infoHandler)
+	errorOption := slog.HandlerOptions{
+		Level: slog.LevelError,
+	}
+	errorHandler := slog.NewTextHandler(os.Stderr, &errorOption)
+	errorLogger := slog.New(errorHandler)
+	return &Log{
+		infoLogger:  infoLogger,
+		errorLogger: errorLogger,
+	}
+}
+
+func (log *Log) Info(msg string) {
+	log.infoLogger.Info(msg)
+}
+
+func (log *Log) Infof(msg string, args ...interface{}) {
+	log.infoLogger.Info(msg, args...)
+}
+
+func (log *Log) Error(msg string) {
+	log.errorLogger.Error(msg)
+}
+
+func (log *Log) Errorf(msg string, args ...interface{}) {
+	log.errorLogger.Error(msg, args...)
 }
